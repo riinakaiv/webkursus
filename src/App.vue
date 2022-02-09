05335bf0463ae26c9dd8de56020d4e6069a18e6a
Riina <riina.kaiv@gmail.com>
2022-02-09 09:43:46 +0200

    Add updates 7.2 class

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  <button v-on:click="clicks++">click me</button> {{clicks}}<br>
-  <input type= "text" v-model="message">
-  <h1>{{ message }}</h1>
-  <br>
-  <h1>Shopping List</h1>
-  <input type="text" v-model="newItem" @keypress.enter="addItem">
-  <button @click="addItem" :disabled="newItem.trim() === ''">Add</button>
-  <ul>
-      <li v-for="(item, index) in items" :key="index">
-          <button @click="items.splice(index, 1)">Delete</button>
-          {{item}} 
-          </li>
-  </ul>
+<div class="container">
+        <section class="section">
+        <button class="button is-primary" @click="clicks++">click me</button> 
+        {{clicks}}
+        <br>
+        <input class="input" type= "text" v-model="message">
+        <h1 class="is-size-1" >{{ message }}</h1>
+         <br>
+         <h1 class="is-size-1">Shopping List</h1>
+         <div class="field has-addons">
+            <div class="control">
+                <input class="input" type="text" placeholder="New item" v-model="newItem" @keypress.enter="addItem">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="addItem" :disabled="newItem.trim() === ''">
+                Add
+                </button>
+            </div>
+        </div>
+         <ul>
+             <li class="mt-2" v-for="(item, index) in items" :key="index">
+                 <button class="button is-primary is-small" @click="items.splice(index, 1)">Delete</button>
+                 {{item}} 
+             </li>
+         </ul>
+      </section>
+</div>
 </template>
 
 
